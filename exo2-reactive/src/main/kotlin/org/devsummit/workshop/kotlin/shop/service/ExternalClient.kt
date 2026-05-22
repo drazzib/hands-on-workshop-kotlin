11f91c6e6a29afbbff413360541c7ac83ef659b8
Zomzog <zomzog@gmail.com>
2026-05-22 14:35:46 +0200

    rework exo2

--- exo2-reactive/src/main/kotlin/org/devsummit/workshop/kotlin/shop/service/ExternalClient.kt
+++ exo2-reactive/src/main/kotlin/org/devsummit/workshop/kotlin/shop/service/ExternalClient.kt
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
-import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.reactive.function.client.awaitBodyOrNull
 
 @Service
 class ExternalClient(private val petNameWebClient: WebClient) {
@@ -49,6 +48,26 @@ class ExternalClient(private val petNameWebClient: WebClient) {
             ?: throw PriceV1UnhandledError(Exception("Empty price response"))
     } catch (e: Exception) {
         throw PriceV1UnhandledError(e)
+    }
+    suspend fun fetchPetPriceV2(kind: PetKind): PetPrice = try {
+        petNameWebClient
+            .get()
+            .uri { it.path("/api/v2/pet-price")
+                .queryParam("kind", kind).build()
+            }
+            .retrieve()
+            .onStatus({ t -> t.isError }) {
+                WebClientApiErrorHelper.createMonoError(it)
+            }
+            .awaitBodyOrNull<RemotePetPriceDto>()
+            ?.let { PetPrice(Price(it.price), Currency(it.currency)) } ?: throw PriceV1UnhandledError(Exception("Empty price response"))
+    } catch (e: HttpClientException) {
+        when (e.statusCode) {
+            HttpStatus.NOT_FOUND -> throw PriceV2NotFoundError(e.message)
+            else -> throw PriceV2UnhandledError(e)
+        }
+    } catch (e: Exception) {
+        throw PriceV2UnhandledError(e)
     }
 
 }
